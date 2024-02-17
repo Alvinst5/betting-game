b64fec8801b0cdac8c6c8c177ab5fd4800e9bfdc
Thims08 <158995194+Thims08@users.noreply.github.com>
2024-02-17 14:25:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function deposit(){
     const Deposit_Amount = document.getElementById("depositAmount").value;
     localStorage.setItem('deposit-amount', Deposit_Amount);
     window.open("bettingpart.html", "_self");
-    
 }
